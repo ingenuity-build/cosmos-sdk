f0ec4387d5a277e7328ca16dc143350e4a4fb20a
aljo242 <alex@shmeeload.xyz>
2023-04-12 10:11:34 -0400

    add extra info

--- x/auth/ante/sigverify.go
+++ x/auth/ante/sigverify.go
@@ -296,9 +296,9 @@ func (svd SigVerificationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simul
 				if OnlyLegacyAminoSigners(sig.Data) {
 					// If all signers are using SIGN_MODE_LEGACY_AMINO, we rely on VerifySignature to check account sequence number,
 					// and therefore communicate sequence number as a potential cause of error.
-					errMsg = fmt.Sprintf("signature verification failed; please verify account number (%d), sequence (%d) and chain-id (%s)", accNum, acc.GetSequence(), chainID)
+					errMsg = fmt.Sprintf("signature verification failed; please verify account number (%d), sequence (%d) and chain-id (%s): sig error %w", accNum, acc.GetSequence(), chainID, err)
 				} else {
-					errMsg = fmt.Sprintf("signature verification failed; please verify account number (%d) and chain-id (%s)", accNum, chainID)
+					errMsg = fmt.Sprintf("signature verification failed; please verify account number (%d) and chain-id (%s): sig error %w\"", accNum, chainID, err)
 				}
 				ctx.Logger().Info("SigVerificationDecorator",
 					"errMsg", errMsg,
